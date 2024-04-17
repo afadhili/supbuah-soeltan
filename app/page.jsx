65207b5bd17d69d5ed0e4a3966eee8caaab887a0
L1HD4F <afadhili03@gmail.com>
2024-04-17 23:13:44 +0700

    add product section

--- app/page.jsx
+++ app/page.jsx
@@ -25,7 +25,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div id="about" className="w-full min-h-screen py-8 px-4 md:px-20 justify-center items-center grid grid-cols-1 md:grid-cols-2 bg-gradient-to-b from-slate-900 to-slate-800">
+      <div id="about" className="my-24 w-full min-h-screen py-8 px-4 md:px-20 justify-center items-center grid grid-cols-1 md:grid-cols-2 bg-gradient-to-b from-slate-900 to-slate-800">
         <div className="flex justify-center">
           <Image src={'/supbuah_vector.webp'} className="" alt="sup buah" width={400} height={400} />
         </div>
@@ -42,18 +42,31 @@ export default function Home() {
         </div>
       </div>
 
-      <div id="product" className="w-full min-h-screen bg-gradient-to-br from-slate-800 via-slate-600 to-slate-900">
+      <div id="product" className="my-24 w-full min-h-screen bg-gradient-to-br from-slate-800 via-slate-600 to-slate-900">
         <div className="bg-[url(/topography.svg)] bg-center">
           <div className="w-full h-screen flex justify-center items-center bg-gradient-to-b from-slate-800 to-transparent">
 
+            <a href="#" className="relative w-full grid grid-cols-1 md:grid-cols-2 mx-4 md:mx-24 rounded-lg shadow-sm bg-gradient-to-r from-pink-600 via-pink-600 to-pink-900">
+              <div className="relative flex flex-col justify-center py-6 px-8">
+                <h1 className="text-4xl text-pink-300 font-bold">Menu Utama</h1>
+                <h1 className="text-4xl text-pink-100 font-bold mb-6">Sup Buah Soeltan</h1>
+                <h1 className="text-xl text-pink-200 font-bold">Get Your Taste!</h1>
 
+                <div className="text-4xl self-end w-min font-bold py-4 px-8 my-3 rounded-full bg-pink-100">10<span className="text-lg">K</span></div>
+              </div>
+
+              <div className="flex justify-center px-6">
+                <Image src={'/supbuah_vector.webp'} className="" alt="sup buah" width={400} height={400} />
+              </div>
+
+            </a>
 
           </div>
         </div>
       </div>
 
       <div id="contact" className="w-full grid grid-cols-1 md:grid-cols-2 bg-gradient-to-tr from-slate-800 via-slate-600 to-slate-900">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15835.53665756908!2d112.60134726762772!3d-7.139391626402176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e77ff004c9f93ad%3A0x8803e0d389b0750a!2sSup%20Buah%20Soeltan!5e0!3m2!1sen!2sid!4v1711922913803!5m2!1sen!2sid" width="100%" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15835.53665756908!2d112.60134726762772!3d-7.139391626402176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e77ff004c9f93ad%3A0x8803e0d389b0750a!2sSup%20Buah%20Soeltan!5e0!3m2!1sen!2sid!4v1711922913803!5m2!1sen!2sid" width="100%" height="300" allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         <div className="w-full h-[300px] flex flex-col justify-center items-center text-center bg-gradient-to-b from-transparent to-slate-900">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
             Sup Buah Soeltan
